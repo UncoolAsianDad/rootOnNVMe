edbbe09bec4c85ec7fe30f694a041919dc832e96
Brian Ho <brian@codeforart.com>
2025-01-11 21:36:04 -0800

    feature: stop the clonine process if the target partition does not exist

--- copy-rootfs-ssd.sh
+++ copy-rootfs-ssd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 # Mount the SSD as /mnt
+# Check if the device exists
+if [ ! -e /dev/nvme0n1p1 ]; then
+	    echo "Error: /dev/nvme0n1p1 does not exist."
+	        exit 1
+fi
 sudo mount /dev/nvme0n1p1 /mnt
 # Copy over the rootfs from the SD card to the SSD
 sudo rsync -axHAWX --numeric-ids --info=progress2 --exclude={"/dev/","/proc/","/sys/","/tmp/","/run/","/mnt/","/media/*","/lost+found"} / /mnt
